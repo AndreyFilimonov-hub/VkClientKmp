f74ee7701f30b353af834fe6ee2529f4238ffeec
Andrey Filimonov <andrfil05@mail.ru>
2026-03-01 20:45:24 +0600

    add theme in manifest and install splash screen

--- composeApp/src/androidMain/kotlin/com/filimonov/vkclientkmp/presentation/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/filimonov/vkclientkmp/presentation/MainActivity.kt
@@ -4,11 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.filimonov.vkclientkmp.data.auth.TokenStorage
+import com.filimonov.vkclientkmp.data.auth.VkAuthManager
+import com.filimonov.vkclientkmp.data.repository.OnboardingRepositoryImpl
 import com.filimonov.vkclientkmp.di.AppComponentTemp
 import com.filimonov.vkclientkmp.presentation.ui.App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         enableEdgeToEdge()
         AppComponentTemp.init(VkAuthManager(), TokenStorage(), OnboardingRepositoryImpl(this@MainActivity))
         super.onCreate(savedInstanceState)
